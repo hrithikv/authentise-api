613bca8f1c55cc7d49a5063a05b12252787fc589
hrithikv <35966198+hrithikv@users.noreply.github.com>
2020-08-29 18:09:09 -0400

    Adding work/tests for GET and PUT/POST on authentise api

--- src/schema.js
+++ src/schema.js
@@ -9,10 +9,10 @@ const schema = yup.object().shape({
     .string()
     .required('Please provide lastName')
     .max(50, 'firstName must be less than 50 characters'),
-  genre: yup
+  detail: yup
     .string()
-    .required('Please provide genre')
-    .oneOf(['COMPSCI', 'COLE', 'SCIENCE'], 'please provide valid genre'),
+    .required('Please provide detail')
+    .oneOf(['COMPSCI', 'COLE', 'SCIENCE'], 'please provide valid detail'),
 });
 
 export default schema;
