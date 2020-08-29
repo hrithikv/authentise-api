8fe88946b77fce2378be68bdadac36b014bde8a4
hrithikv <35966198+hrithikv@users.noreply.github.com>
2020-08-29 17:20:14 -0400

    Updating comments

---
+++ src/middleware.js
@@ -0,0 +1,9 @@
+const errorLogger = (err, req, res, next) => {
+  res.status(err.status || 400).send({ errorMessage: err.message });
+};
+
+const validateToken = (err, req, res, next) => {
+  next();
+};
+
+export { errorLogger, validateToken };
