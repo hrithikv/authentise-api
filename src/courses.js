ab1c66981a9531b32ebb21ead4a2395d19b3f86d
hrithikv <35966198+hrithikv@users.noreply.github.com>
2020-08-29 17:46:04 -0400

    feat(cleanup): prepare for launch

---
+++ src/courses.js
@@ -0,0 +1,25 @@
+export default {
+  datastructures: {
+    firstName: 'Bari',
+    lastName: 'Datastructures',
+    genre: 'COMPSCI',
+  },
+
+  oop: {
+    firstName: 'Frank',
+    lastName: 'Oop',
+    genre: 'COMPSCI',
+  },
+
+  theorycomputation: {
+    firstName: 'Zahran',
+    lastName: 'TheoryComputation',
+    genre: 'Cole',
+  },
+
+  physics: {
+    firstName: 'Michael',
+    lastName: 'Physics',
+    genre: 'SCIENCE',
+  },
+};
