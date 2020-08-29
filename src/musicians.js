8127951a6b9fd0a6adddfd4c754e3046708f65a8
hrithikv <35966198+hrithikv@users.noreply.github.com>
2020-08-29 17:22:53 -0400

    feat(cleanup): prepare for launch

---
+++ src/musicians.js
@@ -0,0 +1,25 @@
+export default {
+  ella: {
+    firstName: 'Ella',
+    lastName: 'Fitzgerald',
+    genre: 'JAZZ',
+  },
+
+  sinatra: {
+    firstName: 'Frank',
+    lastName: 'Sinatra',
+    genre: 'JAZZ',
+  },
+
+  mccartney: {
+    firstName: 'Paul',
+    lastName: 'McCartney',
+    genre: 'ROCK',
+  },
+
+  waters: {
+    firstName: 'Muddy',
+    lastName: 'Waters',
+    genre: 'BLUES',
+  },
+};
